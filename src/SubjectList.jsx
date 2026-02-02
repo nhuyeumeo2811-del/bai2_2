e83ef51e0cf96fd84dc97c702a5facd353e1e6a8
nhuyeumeo2811-del <nhuyeumeo2811@gmail.com>
2026-02-02 10:47:33 +0700

    Add multiple files

---
+++ src/SubjectList.jsx
@@ -0,0 +1,18 @@
+import reactLogo from './assets/react.svg'
+import './SubjectList.css'
+
+function SubjectList(props){
+    return(
+        <div className="subject-container">
+            <h2 className="subject-title">Danh Sách Môn Học</h2>
+            <ul className="subject-list">
+                {props.subjects.map((subject, index) => (
+                    <li className="subject-item" key={index}>
+                        {subject}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+export default SubjectList;
